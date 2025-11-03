83ac12eae9497675e0af50643f027d95af94f837
MD Waliullah <waliullah.godev@gmail.com>
2025-11-03 22:46:02 +0600

    feat: multer upload func

--- src/app/helper/fileUploder.ts
+++ src/app/helper/fileUploder.ts
@@ -1,5 +1,7 @@
-import multer from "multer"
 import path from "path"
+import multer from "multer"
+// import { V2 as cloudinary } from "cloudinary";
+
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -12,3 +14,13 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({ storage: storage })
+
+
+const uploadToCloudinary = async(file: Express.Multer.File) => {
+
+}
+
+
+export const fileUploder = {
+    upload
+}
--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,11 +1,13 @@
 import express from "express"
 import { UserController } from "./user.controller";
+import { fileUploder } from "../../helper/fileUploder";
 
 const router = express.Router()
 
 
 router.post(
     "/create-patient",
+    fileUploder.upload.single('file'),
     UserController.createPatient
 )
 
